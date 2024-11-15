6b7da0d320c462349bd74bf9f73aaaeed3f7908b
Denngrh <badennugraha4@gmail.com>
2024-11-15 10:50:50 +0700

    feat: menambah if bertingkat

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("test")
+def hitung():
+        print("Program Persegi Panjang Menghitung Luas dan Keliling Bangun Datar")
+        print("1. Hitung keliling")
+        print("2. Hitung luas")
+
+        pilihan = input("Masukkan pilihan: ")
+        if pilihan == "1":
+           print("Pilihan 1")
+
+        elif pilihan == "2":
+            print("Pilihan 2")
+
+        else:
+            print("Tidak ada pilihan")
+
+hitung()
